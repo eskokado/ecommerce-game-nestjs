afb61a848c50074e191a5aec02ba299927b4e34d
eskokado <eskokado@gmail.com>
2023-04-23 11:45:40 -0300

    feat: create component pages Storefront

---
+++ src/pages/Storefront/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import MainComponent from "@/components/shared/MainComponent";
+
+const Storefront: React.FC = () => {
+  return (
+    <>
+      <MainComponent/>
+    </>
+  )
+}
+
+export default Storefront
