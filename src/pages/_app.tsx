6a5a468d6d2a8e2337466b1151f978ad218f3f91
eskokado <eskokado@gmail.com>
2023-04-23 09:39:50 -0300

    feat: add framework bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from "next/head";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
